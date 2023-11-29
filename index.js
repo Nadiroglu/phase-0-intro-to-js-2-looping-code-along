5e9f03a3a4cf5190fc595dd775fb96c5f6a8e27f
Nail Nadiroglu <nadiroglunail@gmail.com>
2023-11-29 00:17:13 -0500

    yeah

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names) {
+  let thankYouMessages = [];
+  for (let i = 0; i < names.length; i++) {
+  let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+  thankYouMessages.push(message);
+  }
+  return thankYouMessages;
+  }
+  let names = ['Alice', 'Bob', 'Charlie'];
+let messages = writeCards(names);
+console.log(messages);
+function countDown(startingNumber) {
+  for (let i = startingNumber; i >= 0; i--) {
+  console.log(i);
+  }
+  }
